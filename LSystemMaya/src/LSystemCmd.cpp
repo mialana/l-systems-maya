f13f592988d9ae1db926c4ed428a984d5a9490c9
mialana <aliu@amyliu.dev>
2026-02-02 23:06:32 -0500

    feat: last cmake changes, rudimentary curve

--- LSystemMaya/src/LSystemCmd.cpp
+++ LSystemMaya/src/LSystemCmd.cpp
@@ -1,6 +1,9 @@
 #include "LSystemCmd.h"
 
+#include <maya/MPoint.h>
 #include <maya/MGlobal.h>
+#include <maya/MPointArray.h>
+#include <maya/MFnNurbsCurve.h>
 #include <list>
 
 LSystemCmd::LSystemCmd() : MPxCommand() {}
@@ -9,12 +12,20 @@ LSystemCmd::~LSystemCmd() {}
 
 MStatus LSystemCmd::doIt(const MArgList& args)
 {
-    // message in Maya output window
-    cout << "Implement Me!" << endl;
-    std::cout.flush();
+    MStatus status;
+    MPoint start(0, 0, 0);
+    MPoint end(10, 10, 10);
 
-    // message in scriptor editor
-    MGlobal::displayInfo("Implement Me!");
+    MPointArray points;
+    points.append(start);
+    points.append(end);
+
+    MDoubleArray knots;
+    knots.append(0.0);
+    knots.append(1.0);
+
+    MFnNurbsCurve curve;
+    curve.create(points, knots, 1, MFnNurbsCurve::kOpen, false, false, MObject::kNullObj, &status);
 
     return MStatus::kSuccess;
 }
--- cmake/CoreUtils.cmake
+++ cmake/CoreUtils.cmake
@@ -1,8 +1,12 @@
 # general utility function to make CMake module files visible in VS
-function(_MAKE_SOURCE_GROUP_MSVC target files tree_source prefix)
-    target_sources(${target} PRIVATE ${files})
+function(_MAKE_SOURCE_GROUP_MSVC target files tree_source prefix needs_targeting header_only)
+    if(needs_targeting)
+        target_sources(${target} PRIVATE ${files})
+    endif()
 
-    set_source_files_properties(${files} PROPERTIES HEADER_FILE_ONLY TRUE)
+    if(header_only)
+        set_source_files_properties(${files} PROPERTIES HEADER_FILE_ONLY TRUE)
+    endif()
 
     source_group(TREE ${tree_source} PREFIX ${prefix} FILES ${files})
 endfunction(_MAKE_SOURCE_GROUP_MSVC)
